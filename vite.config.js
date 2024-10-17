2118c707ee683d8a1872df39b95c944829d01df2
ankit kumar <128667638+ankitkr80@users.noreply.github.com>
2024-10-17 10:08:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Exposes the app to the local network
+    port: 3000        // Specify a custom port (default is 5173, you can change to any available port)
+  }
 })
